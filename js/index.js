63c17483529e4dd11ad75b31b2a4b688c17407c6
David Arevalo <davidarevaloc20@gmail.com>
2021-04-22 10:02:59 -0500

    javaScript

--- js/index.js
+++ js/index.js
@@ -2,11 +2,7 @@ const email = document.getElementById("form_mail")
 const pass = document.getElementById("form_password")
 const form = document.getElementById("form")
 const parrafo = document.getElementById("warnings")
-form.addEventListener("submit", e=>{
-  e.preventDefault()
-  let regexEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 
-  console.log(regexEmail.test(email.value))
-  if (regexEmail.test(email.value)) {
-  }
-})
+const mostrarValores = () =>{
+  alert("HOla")  
+} 
--- js/resgistrate.js
+++ js/resgistrate.js
@@ -1,9 +1,21 @@
-const form = document.getElementById("form")
+const form = document.getElementById("form");
+const inputs = document.querySelectorAll("#form input");
+const name = document.getElementById("form_name");
 const pass = document.getElementById("form_registrarse-password");
-const passRepet = document.getElementById("form_registrarse-password2");
-
-form.addEventListener("submit", e=>{
-  if (pass.value===passRepet) {
-    console.log ("Si")
+const pass2 = document.getElementById("form_registrarse-password2");
+const expresiones = {
+	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, 
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, 
+	password: /^.{4,12}$/, 
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/ 
+};
+pass2.addEventListener("input",()=>{
+  if(pass.value===pass2.value){
+    document.getElementById('correcto').classList.add('correcto2');
+    document.getElementById('errado').classList.remove('errado2');
+  }  else {
+    document.getElementById('errado').classList.add('errado2');
+    document.getElementById('correcto').classList.remove('correcto2');
   }
 })
